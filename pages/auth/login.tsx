be58513c9552d01f64b7d0b7778f1a68315823d0
Fernando <fernandomercado529@gmail.com>
2022-04-05 13:04:07 -0400

    Template

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -5,6 +5,7 @@ import NextLink from 'next/link'
 const LoginPage = () => {
   return (
       <AuthLayout title="Login">
+          {/* This is a template */}
           <Box sx={{width:350,padding:'10px 20px'}}>
               <Grid container spacing={2}>
                   <Grid item xs={12} display='flex' justifyContent='center'>
